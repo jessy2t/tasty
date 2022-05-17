fd62f34d0c7ab5475fef305008c47fbc8a0ac17d
Jessy Tieu <jessytieu@gmx.de>
2022-05-17 10:35:43 +0200

    Navigation zu Start des Bestellenden

--- app/src/main/java/com/acme/tasty/MainActivity.java
+++ app/src/main/java/com/acme/tasty/MainActivity.java
@@ -1,23 +1,26 @@
 package com.acme.tasty;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.acme.testy.CUSTOMER_MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        addOnClickListenerToButton(R.id.restaurant_btn);
-        addOnClickListenerToButton(R.id.customer_btn);
     }
 
-    private void addOnClickListenerToButton(int id) {
-        Button button = findViewById(id);
-        button.setOnClickListener(v -> button.setText("Clicked!"));
+    }
+
+    public void navigateToCustomerStart(View view){
+        Intent intent = new Intent(this, CustomerStartActivity.class);
+        Button btn = findViewById(R.id.customer_btn);
+        String message = btn.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
