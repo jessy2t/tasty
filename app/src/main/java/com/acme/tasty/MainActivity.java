3ea2a6fc8fcaf30745f9c17d44a8a508fbc926ca
Jessy Tieu <jessytieu@gmx.de>
2022-05-17 10:38:14 +0200

    Navigation zu Login Activity

--- app/src/main/java/com/acme/tasty/MainActivity.java
+++ app/src/main/java/com/acme/tasty/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.support.v7.app.AppCompatActivity;
+import com.acme.tasty.ui.login.RestaurantLoginActivity;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.acme.testy.CUSTOMER_MESSAGE";
@@ -14,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void navigateToRestaurantLogin(View view){
+        Intent intent = new Intent(this, RestaurantLoginActivity.class);
+        startActivity(intent);
     }
 
     public void navigateToCustomerStart(View view){
