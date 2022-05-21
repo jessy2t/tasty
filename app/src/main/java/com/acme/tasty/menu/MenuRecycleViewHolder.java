e06df9240166b7993cdacfb688c4f5f001f34e63
cluedia <claudia.hahn@gmail.com>
2022-05-21 11:31:52 +0200

    Add customer menu and starting page

---
+++ app/src/main/java/com/acme/tasty/menu/MenuRecycleViewHolder.java
@@ -0,0 +1,11 @@
+package com.acme.tasty.menu;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class MenuRecycleViewHolder extends RecyclerView.ViewHolder {
+    public MenuRecycleViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
