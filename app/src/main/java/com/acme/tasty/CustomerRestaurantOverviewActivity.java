0d9a56fb66bea824b2dfaf6c1c6bd57d6e7dd2f0
Jessy Tieu <jessytieu@gmx.de>
2022-05-25 16:56:35 +0200

    Tabs zur Navigation der Restaurantübersicht hinzugefügt
    # Conflicts:
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/acme/tasty/CustomerRestaurantOverviewActivity.java
+++ app/src/main/java/com/acme/tasty/CustomerRestaurantOverviewActivity.java
@@ -1,13 +1,76 @@
 package com.acme.tasty;
 
-import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import android.os.Bundle;
+import com.acme.tasty.fragments.CustomerRestaurantOverviewGeneralFragment;
+import com.acme.tasty.fragments.CustomerRestaurantOverviewRatingsFragment;
+import com.google.android.material.tabs.TabLayout;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomerRestaurantOverviewActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private CustomerRestaurantOverviewGeneralFragment generalFragment;
+    private CustomerRestaurantOverviewRatingsFragment ratingsFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_restaurant_overview);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = findViewById(R.id.view_pager);
+        tabLayout = findViewById(R.id.tab_layout);
+        generalFragment = new CustomerRestaurantOverviewGeneralFragment();
+        ratingsFragment = new CustomerRestaurantOverviewRatingsFragment();
+        tabLayout.setupWithViewPager(viewPager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), 0);
+        viewPagerAdapter.addFragment(generalFragment,"");
+        viewPagerAdapter.addFragment(ratingsFragment,"");
+        viewPager.setAdapter(viewPagerAdapter);
+
+        tabLayout.getTabAt(0).setText("Übersicht");
+        tabLayout.getTabAt(1).setText("Bewertungen");
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+        private List<Fragment> fragments = new ArrayList<>();
+        private List<String> fragmentTitles = new ArrayList<>();
+        public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+            super(fm, behavior);
+        }
+
+        public void addFragment(Fragment fragment, String title){
+            fragments.add(fragment);
+            fragmentTitles.add(title);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitles.get(position);
+        }
     }
 }
---
+++ app/src/main/java/com/acme/tasty/fragments/CustomerRestaurantOverviewRatingsFragment.java
@@ -0,0 +1,20 @@
+package com.acme.tasty.fragments;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.acme.tasty.R;
+import org.jetbrains.annotations.NotNull;
+
+public class CustomerRestaurantOverviewRatingsFragment extends Fragment {
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_customer_restaurant_overview_ratings, container, false);
+    }
+}
