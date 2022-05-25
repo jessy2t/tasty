d2d7ed2f1c69195fe7af58409f7fef89bd1de831
cluedia <claudia.hahn@gmail.com>
2022-05-25 18:06:15 +0200

    fix androidx

--- app/src/main/java/com/acme/tasty/menu/MenuRecycleViewHolder.java
+++ app/src/main/java/com/acme/tasty/menu/MenuRecycleViewHolder.java
@@ -1,8 +1,9 @@
 package com.acme.tasty.menu;
 
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MenuRecycleViewHolder extends RecyclerView.ViewHolder {
     public MenuRecycleViewHolder(@NonNull View itemView) {
--- app/src/main/java/com/acme/tasty/menu/MenuRecyclerViewAdapter.java
+++ app/src/main/java/com/acme/tasty/menu/MenuRecyclerViewAdapter.java
@@ -1,17 +1,16 @@
 package com.acme.tasty.menu;
 
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MenuRecyclerViewAdapter extends RecyclerView.Adapter<MenuRecycleViewHolder> {
     @NonNull
-    @Override
     public MenuRecycleViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         return null;
     }
 
-    @Override
     public void onBindViewHolder(@NonNull MenuRecycleViewHolder menuRecycleViewHolder, int i) {
 
     }
