57e0c9c059b50b0becc5771d6596b78c02a8b321
Mogli168 <glmo1011@h-ka.de>
2022-06-01 16:07:22 +0200

    Nav bar integriert

--- app/src/main/java/com/acme/tasty/CustomerRecommendationActivity.java
+++ app/src/main/java/com/acme/tasty/CustomerRecommendationActivity.java
@@ -3,17 +3,30 @@ package com.acme.tasty;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import com.acme.tasty.popup.PopupWindowService;
 
 public class CustomerRecommendationActivity extends AppCompatActivity {
     private PopupWindow _popupWindow;
+    private Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_recommendation);
+
+        mToolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 
     public void clickOnRestaurantRecommendation(View view) {
--- app/src/main/java/com/acme/tasty/MainActivity.java
+++ app/src/main/java/com/acme/tasty/MainActivity.java
@@ -2,15 +2,29 @@ package com.acme.tasty;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import com.acme.tasty.ui.login.RestaurantLoginActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 
     public void navigateToRestaurantLogin(View view){
