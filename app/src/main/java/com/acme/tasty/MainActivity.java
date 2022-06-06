7a61827f5fa3556006d21996791415b86ca9f054
Dinh Thien Phan <dithienphan@gmail.de>
2022-06-06 21:36:15 +0200

    preferences

--- app/src/main/java/com/acme/tasty/MainActivity.java
+++ app/src/main/java/com/acme/tasty/MainActivity.java
@@ -3,6 +3,8 @@ package com.acme.tasty;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.acme.tasty.ui.login.RestaurantLoginActivity;
 
@@ -10,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_praeferenzen_uebersicht);
     }
 
     public void navigateToRestaurantLogin(View view){
@@ -21,5 +23,21 @@ public class MainActivity extends AppCompatActivity {
     public void navigateToCustomerStart(View view){
         Intent intent = new Intent(this, CustomerStartActivity.class);
         startActivity(intent);
+    }
+
+    public void onCheckboxClicked(View view) {
+
+        boolean checked = ((CheckBox) view).isChecked();
+
+        switch(view.getId()) {
+            case R.id.checkbox_indisch:
+                if (checked);
+                else
+                    break;
+            case R.id.checkbox_mexikanisch:
+                if (checked);
+                else
+                    break;
+        }
     }
 }
