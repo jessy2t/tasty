fd3b4392f7bec514f69ec3cb2162efdff1e0f9bf
l3cs0 <101865730+l3cs0@users.noreply.github.com>
2022-06-09 14:56:10 +0200

    variablen zu input RestaurantRegistration1Activity

--- app/src/main/java/com/acme/tasty/RestaurantRegistration1Activity.java
+++ app/src/main/java/com/acme/tasty/RestaurantRegistration1Activity.java
@@ -8,102 +8,52 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.acme.tasty.databaseHelpers.AddressDBHelper;
-import com.acme.tasty.databaseHelpers.CategoriesDBHelper;
-import com.acme.tasty.databaseHelpers.CityDBHelper;
-import com.acme.tasty.databaseHelpers.RestaurantAttributesDBHelper;
-import com.acme.tasty.databaseHelpers.RestaurantDBHelper;
-
 public class RestaurantRegistration1Activity extends AppCompatActivity {
 
     protected EditText nameRestaurant;
-    protected EditText strasse;
-    protected EditText ort;
+    protected EditText strasseRestaurant;
+    protected EditText hausnummerRestaurant;
+    protected EditText plzRestaurant;
+    protected EditText ortRestaurant;
     protected CheckBox checkBoxLieferServiceVorhanden;
     protected CheckBox checkBoxReservierungMoeglich;
     protected CheckBox checkBoxReservierungNotwendig;
     protected CheckBox checkBoxInAppBezahlung;
     protected CheckBox checkBoxVegetarisch;
     protected CheckBox checkBoxVegan;
-    protected CheckBox checkBoxIndisch;
-    protected CheckBox checkBoxIndonesisch;
+    protected CheckBox checkBoxAsiatisch;
+    protected CheckBox checkBoxJapanisch;
+    protected CheckBox checkBoxEuropaeisch;
     protected CheckBox checkBoxItalienisch;
-    protected CheckBox checkBoxDeutsch;
+    protected CheckBox checkBoxGriechisch;
     protected CheckBox checkBoxMexikanisch;
-    protected CheckBox checkBoxAmerikanisch;
-    protected CheckBox checkBoxChinesisch;
-    private RestaurantAttributesDBHelper restaurantAttributesDBHelper;
-    private CategoriesDBHelper categoriesDBHelper;
-    private AddressDBHelper AddressDB;
-    private RestaurantDBHelper RestaurantDB;
-    private CityDBHelper CityDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_restaurant_registration_1);
-    }
-    public void navigateToRestaurantRegistration2(View view){
+        setContentView(R.layout.activity_restaurant_registration1);
+
         nameRestaurant = findViewById(R.id.nameRestaurant);
-        strasse = findViewById(R.id.straßeRestaurant);
-        ort = findViewById(R.id.plzRestaurant);
+        strasseRestaurant = findViewById(R.id.strasseRestaurant);
+        ortRestaurant = findViewById(R.id.ortRestaurant);
+        plzRestaurant = findViewById(R.id.plzRestaurant);
+        hausnummerRestaurant = findViewById(R.id.hausnummerRestaurant);
+
         checkBoxLieferServiceVorhanden = findViewById(R.id.checkBoxLieferServiceVorhanden);
-        checkBoxReservierungMoeglich = findViewById(R.id.checkBoxReservierungMöglich);
+        checkBoxReservierungMoeglich = findViewById(R.id.checkBoxReservierungMoeglich);
         checkBoxReservierungNotwendig = findViewById(R.id.checkBoxReservierungNotwendig);
         checkBoxInAppBezahlung = findViewById(R.id.checkBoxInAppBezahlung);
         checkBoxVegetarisch = findViewById(R.id.checkBoxVegetarisch);
         checkBoxVegan = findViewById(R.id.checkBoxVegan);
-        checkBoxIndisch = findViewById(R.id.checkBoxIndisch);
-        checkBoxIndonesisch = findViewById(R.id.checkBoxIndonesisch);
+        checkBoxAsiatisch = findViewById(R.id.checkBoxAsiatisch);
+        checkBoxJapanisch = findViewById(R.id.checkBoxJapanisch);
+        checkBoxEuropaeisch = findViewById(R.id.checkBoxEuropaeisch);
         checkBoxItalienisch = findViewById(R.id.checkBoxItalienisch);
-        checkBoxDeutsch = findViewById(R.id.checkBoxDeutsch);
+        checkBoxGriechisch = findViewById(R.id.checkBoxGriechisch);
         checkBoxMexikanisch = findViewById(R.id.checkBoxMexikanisch);
-        checkBoxAmerikanisch = findViewById(R.id.checkBoxAmerikanisch);
-        checkBoxChinesisch = findViewById(R.id.checkBoxChinesisch);
- /*
-        AddressDB = new AddressDBHelper(this);
-        CityDB = new CityDBHelper(this);
-        RestaurantDB = new RestaurantDBHelper(this);
 
-        Ort und Straße sind zurzeit nur ein Feld deshalb müssen Sie mit leerzeichen getrennt eingegeben werden
-        76133 Karlsruhe
-        Hauptstraße 1
-
-        String zip = ort.getText().toString().split(" ")[0];
-        String city = ort.getText().toString().split(" ")[1];
-
-        String name = strasse.getText().toString().split(" ")[0];
-        Integer number = Integer.valueOf(strasse.getText().toString().split(" ")[1]);
-
-        CityDB.insertData(zip, city);
-
-        Integer addressId = AddressDB.insertData(name,number,zip);
-        if (addressId == 0)
-            return;
-
-        Integer catId = categoriesDBHelper.getId(
-                checkBoxMexikanisch.isChecked(),
-                checkBoxIndisch.isChecked(),
-                checkBoxIndonesisch.isChecked(),
-                checkBoxItalienisch.isChecked(),
-                checkBoxDeutsch.isChecked(),
-                checkBoxAmerikanisch.isChecked(),
-                checkBoxChinesisch.isChecked()
-        );
-
-        Integer attributesId = restaurantAttributesDBHelper.getId(
-                checkBoxLieferServiceVorhanden.isChecked(),
-                checkBoxReservierungMoeglich.isChecked(),
-                checkBoxReservierungNotwendig.isChecked(),
-                checkBoxInAppBezahlung.isChecked(),
-                checkBoxVegetarisch.isChecked(),
-                checkBoxVegan.isChecked(),
-                catId
-        );
-
-        RestaurantDB.insertData(nameRestaurant.getText().toString(),attributesId,addressId);
-        */
-
+    }
+    public void navigateToRestaurantRegistration2(View view){
         Intent intent = new Intent(this, RestaurantRegistration2Acitivity.class);
         startActivity(intent);
         finish();
