a261a618ab7cb9a083d8081be5a5dde6dffc3bc5
l3cs0 <101865730+l3cs0@users.noreply.github.com>
2022-06-14 20:01:27 +0200

    Restaurantregistration1 & 2

--- app/src/main/java/com/acme/tasty/RestaurantRegistration2Acitivity.java
+++ app/src/main/java/com/acme/tasty/RestaurantRegistration2Acitivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class RestaurantRegistration3Acitivity extends AppCompatActivity {
+public class RestaurantRegistration2Acitivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_restaurant_registration3);
+        setContentView(R.layout.activity_restaurant_registration1);
     }
 }
