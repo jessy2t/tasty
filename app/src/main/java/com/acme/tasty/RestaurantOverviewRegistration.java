ff69add688fa58fefdd0419d009531609eaff7b9
l3cs0 <101865730+l3cs0@users.noreply.github.com>
2022-06-15 17:47:24 +0200

    RestaurantOverviewRegistration hinzugefügt

---
+++ app/src/main/java/com/acme/tasty/RestaurantOverviewRegistration.java
@@ -0,0 +1,14 @@
+package com.acme.tasty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestaurantOverviewRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_overview_registration);
+    }
+}
