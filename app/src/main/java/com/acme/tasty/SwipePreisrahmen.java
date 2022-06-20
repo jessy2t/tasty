1a702f8f44c044d866700949295f3e8bd5d037e3
cluedia <claudiahahn00@gmail.com>
2022-06-20 20:18:43 +0200

    add styling of swipes and menu popup

--- app/src/main/java/com/acme/tasty/SwipePreisrahmen.java
+++ app/src/main/java/com/acme/tasty/SwipePreisrahmen.java
@@ -3,6 +3,8 @@ package com.acme.tasty;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.SeekBar;
